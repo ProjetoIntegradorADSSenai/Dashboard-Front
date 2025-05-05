10e2ae3230eabafe8625bdbd8e3138cba9586727
Felipe <felipemarques.computacao@gmail.com>
2025-05-05 20:00:15 -0300

    feat: change project name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/AppSideBar";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Kanedo e os P. Diddys",
+  title: "Projeto Integrador",
   description: "projeto integrador",
 };
 
@@ -26,15 +22,15 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
- 
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <main >
-        {children}
-      </main>
+        <main >
+          {children}
+        </main>
       </body>
     </html>
   );
