6e96c1b619e5fc7c65baebc7d9cdcca1f4541cef
Felipe <felipemarques.computacao@gmail.com>
2025-05-05 20:00:48 -0300

    feat: adjust login button

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -4,9 +4,9 @@ import { signIn } from "next-auth/react";
 import { Button } from "./ui/button";
 
 export default function LoginButton() {
-    return (
-        <Button className="rounded-full p-6" onClick={() => signIn("github", {callbackUrl: "/"})}>
-            Entrar com o Google
-        </Button>
-    )
+  return (
+    <Button className="rounded-full p-6" onClick={() => signIn("github", { callbackUrl: "/" })}>
+      Entrar com o GitHub
+    </Button>
+  )
 }
