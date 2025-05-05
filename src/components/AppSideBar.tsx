47236025404ca2c1e486f01223f4da7a4c25fce4
Felipe <felipemarques.computacao@gmail.com>
2025-05-05 20:17:57 -0300

    feat: better formatting

--- src/components/AppSideBar.tsx
+++ src/components/AppSideBar.tsx
@@ -3,43 +3,43 @@ import { Button } from "./ui/button"
 import LogoutButton from "./LogoutButton"
 
 const navItens = [{
-  name : "Dashboard",
-  url : "/"
+  name: "Dashboard",
+  url: "/"
 },
 {
-  name : "Plasticos",
-  url : "/Plasticos"
+  name: "Plásticos",
+  url: "/Plasticos"
 },
 {
-  name : "Metalicos",
-  url : "/Metalicos"
+  name: "Metálicos",
+  url: "/Metalicos"
 },
 ]
 
 export function AppSidebar() {
-    return (
-        <Sidebar className="w-[15%] m-0">
-          <SidebarContent>
-            <SidebarGroup>
-              <SidebarGroupLabel>Kanedos</SidebarGroupLabel>
-              <SidebarGroupContent>
-                <SidebarMenu>
-                  {navItens.map((item, index) => (
-                    <SidebarMenuItem key={index}>
-                      <SidebarMenuButton asChild>
-                        <a href={ `${item.url}`}>
-                          <span>{item.name}</span>
-                        </a>
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  ))}
-                  <SidebarMenuItem>
-                    <LogoutButton />
-                  </SidebarMenuItem>
-                </SidebarMenu>
-              </SidebarGroupContent>
-            </SidebarGroup>
-          </SidebarContent>
-        </Sidebar>
-      )
+  return (
+    <Sidebar className="w-[15%] m-0">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Kanedos</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {navItens.map((item, index) => (
+                <SidebarMenuItem key={index}>
+                  <SidebarMenuButton asChild>
+                    <a href={`${item.url}`}>
+                      <span>{item.name}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+              <SidebarMenuItem>
+                <LogoutButton />
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
 }
